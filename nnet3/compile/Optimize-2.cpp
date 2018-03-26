42c67522e23270b1d7b83b120af3ca52ebe46159
nan <liu.junnan@outlook.com>
2018-03-26 21:01:47 +0800

    d

--- nnet3/compile/Optimize-2.cpp
+++ nnet3/compile/Optimize-2.cpp
@@ -57,7 +57,9 @@ bool VariableMergingOptimizer::MergeVariables() {
     return false;
   bool merged = false;
   int32 num_commands = computation_->commands.size();
+  
   for (int32 command_index = 0; command_index < num_commands; command_index++) {
+
     // 这个循环目的是查找 pair <s1, s2> , 可能被merge到一个variable中的 submatrix对.
     const NnetComputation::Command &c = computation_->commands[command_index];
     int32 s1 = -1, s2 = -1;
@@ -68,31 +70,27 @@ bool VariableMergingOptimizer::MergeVariables() {
         config_.remove_assignments) {
       s2 = c.arg1;  // s2 is the written-to matrix.
       s1 = c.arg2;
-    } else if (c.command_type == kPropagate &&
-               config_.propagate_in_place) {
+    }
+
+
+    else if (c.command_type == kPropagate &&  config_.propagate_in_place) {
       const Component *component = nnet_.GetComponent(c.arg1);
       if (component->Properties() & kPropagateInPlace) {
         s1 = c.arg3;
         s2 = c.arg4;  // s2 is the written-to matrix.
       }
-    } else if ((c.command_type == kBackprop ||
-                c.command_type == kBackpropNoModelUpdate) &&
-               config_.backprop_in_place) {
+    }
+
+
+    else if ((c.command_type == kBackprop || c.command_type == kBackpropNoModelUpdate) &&  config_.backprop_in_place) {
       const Component *component = nnet_.GetComponent(c.arg1);
       if (component->Properties() & kBackpropInPlace) {
         s1 = c.arg5;
         s2 = c.arg6;  // s2 is the written-to matrix.
-        if (s1 == c.arg3 || s2 == c.arg3 || s1 == c.arg4 || s2 == c.arg4) {
-          // we don't think this should ever happen, but just out of an
-          // abundance of caution: if either of these submatrix indexes are the
-          // input-value or output-value args to Backprop, don't do the optimization.
-          s1 = -1;
-          s2 = -1;
-        }
       }
     }
 
-
+    
     // 
     if (s1 > 0 && s2 > 0) {
       // MayBeMerged 内部判断所有 7 个condition 如果都通过, 则认为可以进行merge合并.
@@ -132,14 +130,14 @@ std::pair<bool,bool> VariableMergingOptimizer::MayBeMerged(
   // 将s1 s2 submatrix 中的variable 都加入到 variable_indexes中
   analyzer_.variables.AppendVariablesForSubmatrix(s1, &variable_indexes);
   analyzer_.variables.AppendVariablesForSubmatrix(s2, &variable_indexes);
+
   std::vector<int32>::iterator iter = variable_indexes.begin(),
       end = variable_indexes.end();
-
-
   // condition c5:
   for (; iter != end; ++iter)
     if (variable_dirty_[*iter])
       return std::pair<bool,bool>(false,false);
+
   
   const std::vector<MatrixAccesses> &matrix_accesses = analyzer_.matrix_accesses;
   const MatrixAccesses &m1_access = matrix_accesses[m1],
@@ -159,6 +157,8 @@ std::pair<bool,bool> VariableMergingOptimizer::MayBeMerged(
   
   bool left = config_.allow_left_merge,
       right = config_.allow_right_merge;
+
+  // 肯定至少一个是WholeMatrix
   // condition c3:
   if (!computation_->IsWholeMatrix(s2)) left = false;
 
@@ -207,15 +207,18 @@ std::pair<bool,bool> VariableMergingOptimizer::MayBeMerged(
    当然 matrix-index 将会是 b的 matrix_index,    但是我们可能必须修改row 和 col offset.
    这个想法是 submatrix submat_b 应该具有相同的维度 与 submat_a的潜在matrix矩阵.
  */
+
+// submat_a --- one_s_to_discard
+// submat_b --- s_to_keep
 static NnetComputation::SubMatrixInfo GetSubMatrixOfSubMatrix(
     const NnetComputation &computation, int32 submat_a, int32 submat_b) {
-  KALDI_ASSERT(static_cast<size_t>(submat_a) < computation.submatrices.size());
-  KALDI_ASSERT(static_cast<size_t>(submat_b) < computation.submatrices.size());
+  
   const NnetComputation::SubMatrixInfo &a = computation.submatrices[submat_a],
                                        &b = computation.submatrices[submat_b];
-  const NnetComputation::MatrixInfo &a_mat =
-      computation.matrices[a.matrix_index];
+  
+  const NnetComputation::MatrixInfo &a_mat =   computation.matrices[a.matrix_index];
 
+  // 哪里确定的条件?？？？ 我曹！
   // 这是前面必须满足的条件么?
   KALDI_ASSERT(a_mat.num_rows == b.num_rows && a_mat.num_cols == b.num_cols);
   
@@ -242,18 +245,21 @@ void VariableMergingOptimizer::DoMerge(int32 command_index,
   MarkAsDirty(s_to_keep);
   MarkAsDirty(s_to_discard);
 
-  int32 m_to_keep = computation_->submatrices[s_to_keep].matrix_index,
+  int32
+      m_to_keep = computation_->submatrices[s_to_keep].matrix_index,
       m_to_discard = computation_->submatrices[s_to_discard].matrix_index;
   KALDI_ASSERT(m_to_keep != m_to_discard && m_to_keep > 0 && m_to_discard > 0);
 
-  // 修改m_to_discard的子矩阵 快速成为s_to_keep子矩阵.
+  // m_to_discard 矩阵的 所有子矩阵submatrix 
   {
     std::vector<int32>::const_iterator
         iter = matrix_to_submatrix_[m_to_discard].begin(),
         end = matrix_to_submatrix_[m_to_discard].end();
 
     // 对m_to_discard的所有submatrix 都根据 s_to_keep 生成一个 submatrix.
-    // matrix_index 保存 m_to_keep.
+    // 保存会原地, 实际上就是将原本 以m_to_discard matrix为数据的submatrix
+    // 都改变为 以 m_to_keep的matrix为数据。
+    // 这样就将m_to_discard的matrix 删掉了??????
     for (; iter != end; ++iter) {
       int32 submatrix_index = *iter;
       computation_->submatrices[submatrix_index] = GetSubMatrixOfSubMatrix(*computation_, submatrix_index, s_to_keep);
@@ -268,7 +274,7 @@ void VariableMergingOptimizer::DoMerge(int32 command_index,
 
   // 如果是个 matrixCopy命令, 可以将 matrixCopy命令直接变为 no-op命令
   // 如果是个 matrixCopy+sacle命令, 保留命令,
-  //  它会具有 加权矩阵的作用(会被映射 arg1=arg2??)
+  // 它会具有 加权矩阵的作用(会被映射 arg1=arg2??)
   if (c.command_type == kMatrixCopy && c.alpha == 1.0) {
     // remove the command.
     c.command_type = kNoOperation;
@@ -296,24 +302,14 @@ void VariableMergingOptimizer::DoMerge(int32 command_index,
   {
     // m_to_keep 和 m_to_discard都会具有 allocate command
     // 因为每个matrix都会进行申请空间, 如果其中一个matrix是 kAacceptINput, 就删除另外一个
-    // 因为kAcceptInput命令的位置很重要?
-    // 否则删除discard的那个命令. 所谓之前的一个简化, 如果discard的allocate命令是 kAccept
-    //   - Both m_to_keep and m_to_discard will have commands that allocate
-    //     them, as all matrices do (note, kAcceptInput counts as an allocation
-    //     command).  If one of them is kAcceptInput, then delete the other one,
-    //     because the position of the kAcceptInput commands is important.
-    //     Otherwise delete the "discard" one.  As a simplification of the logic
-    //     of the previous sentence: if the "discard" allocate command is
-    //     kAcceptInput then delete the "keep" allocate command, else delete
-    //     the "discard" allocate command.
-    //     Note: after we renumber the submatrices, they both refer to the
-    //     same underlying matrix, but we need to refer to them using a
-    //     submatrix that refers to the entire matrix.  The one we keep will
-    //     always refer to the entire matrix.  (In the case where one of
-    //     them is an input, both submatrices are guaranteed to refer to the
-    //     entire matrix, this is guaranteed by the logic we use to decide
-    //     which matrices we can merge).
-    int32 alloc_keep = matrix_accesses[m_to_keep].allocate_command,
+    // 因为kAcceptInput命令的位置很重要? 否则删除discard的那个命令.
+    // 一个简化：
+    // 如果discard的allocate命令是 kAcceptInput， 删除kepp的allocate命令, 否则删除discard的allocate命令
+    // 注意, 后面我们会对submatrix重新编号, 他们都会应用相同的matrix， 但是我们需要使用submatrix 引用完整matrix
+    // 我们保留的那个 会一直引用完整matrix， 在这样情况下, 他们中一个是输入， 两个submatrix 都确保引用完整matrix
+    // 这是通过我们用来决定那个matrix可以被merge的逻辑确定了的。
+    int32
+        alloc_keep = matrix_accesses[m_to_keep].allocate_command,
         alloc_discard = matrix_accesses[m_to_discard].allocate_command;
 
     KALDI_ASSERT(alloc_keep != -1 && alloc_discard != -1);
@@ -331,6 +327,9 @@ void VariableMergingOptimizer::DoMerge(int32 command_index,
       discard_alloc_command.command_type = kNoOperation;
       matrix_whose_zeroing_to_discard = m_to_discard;
     }
+
+
+    // 现在移除命令将其中一个矩阵（我们刚刚丢弃了allocate命令的矩阵）归零的命令， 
     // Now remove the command that zeroed one of the matrices
     // (the one whose allocation command we just discarded).
     int32 zeroing_command_to_discard =
